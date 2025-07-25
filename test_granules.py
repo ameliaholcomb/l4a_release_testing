c72b528e237ffa50623df1fb0eeabb9e9e369438
Amelia Holcomb <amelia.holcomb@gmail.com>
2025-07-25 15:41:13 -0400

    initial commit

---
+++ test_granules.py
@@ -0,0 +1,334 @@
+import h5py
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+import pathlib
+import random
+import sys
+import unittest
+
+from tools.granule import Granule
+from tools.granule_name import parse_granule_filename
+
+LOCALFILES_DIR = pathlib.Path(
+    "/Users/amelia/Repos/testdata/gedi_l4a_test_granules"
+)
+V3FILES_DIR = pathlib.Path(
+    "/gpfs/data1/vclgp/armstonj/gedi_l4a_processing/gedi_l4a_r003_test"
+)
+V2FILES_DIR = pathlib.Path("/gpfs/data1/vclgp/data/iss_gedi/soc/2022/132")
+
+TESTDIR = pathlib.Path(__file__).parent
+DATA_DICTIONARY_DIR = TESTDIR / "data_dictionaries"
+
+
+def _get_files_umd():
+    v3files = list(V3FILES_DIR.iterdir())
+    v3md = [parse_granule_filename(f.name) for f in v3files]
+    v2f = [
+        list(
+            V2FILES_DIR.glob(
+                f"GEDI04_A*{md.orbit}_{md.sub_orbit_granule}*_V002.h5"
+            )
+        )
+        for md in v3md
+    ]
+    v2files = []
+    for f in v2f:
+        if len(f) == 2:
+            if f[0] > f[1]:
+                v2files.append(f[0])
+            else:
+                v2files.append(f[1])
+        elif len(f) == 1:
+            v2files.append(f[0])
+        else:
+            print("Error: unexpected number of files found\n", f)
+    return list(zip(v2files, v3files))
+
+
+def _get_files_local():
+    v3files = sorted(list(LOCALFILES_DIR.glob("*V003.h5")))
+    v2files = sorted(list(LOCALFILES_DIR.glob("*V002.h5")))
+    return list(zip(v2files, v3files))
+
+def _get_columns(file_path: pathlib.Path):
+    columns = []
+    with open(file_path, "r") as f:
+        for line in f:
+            columns.append(line.strip())
+    return columns
+
+
+class TestGranule(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.seed = 42
+        random.seed(cls.seed)
+        # This is kinda hacky, but okay
+        try:
+            file_pairs = _get_files_local()
+        except FileNotFoundError:
+            file_pairs = _get_files_umd()
+
+        # Get verbosity level (default to 1 if not available)
+        cls.verbosity = getattr(sys.modules[__name__], "_verbosity", 1)
+
+        # Pick a random subset of file pairs for testing
+        # cls.file_pairs = random.sample(file_pairs, k=5)
+
+        cls.file_pairs = file_pairs
+
+    def test_all_shots_present(self):
+        for v2file, v3file in self.file_pairs:
+            if self.verbosity >= 2:
+                print(f"Testing {v2file.name} and {v3file.name}")
+            columns = ["shot_number"]
+            v2_granule = Granule(v2file, columns)
+            v3_granule = Granule(v3file, columns)
+
+            # Check if all shots in V3 are present in V2
+            v2_shots = set(v2_granule.data["shot_number"])
+            v3_shots = set(v3_granule.data["shot_number"])
+
+            self.assertTrue(
+                v3_shots.issubset(v2_shots),
+                f"Not all V3 shots are present in V2 for {v2file.name} and {v3file.name}",
+            )
+            self.assertTrue(
+                v2_shots.issubset(v3_shots),
+                f"Not all V2 shots are present in V3 for {v2file.name} and {v3file.name}",
+            )
+
+    def test_biomass_pi(self):
+        for _, v3file in self.file_pairs:
+            if self.verbosity >= 2:
+                print(f"Testing biomass prediction interval for {v3file.name}")
+            columns = [
+                "shot_number",
+                "agbd",
+                "agbd_se",
+                "agbd_pi_upper",
+                "agbd_pi_lower",
+            ]
+            v3_granule = Granule(v3file, columns=columns)
+
+            # Check agbd_pi_lower is less than agbd, less than agbd_pi_upper
+            v3_agbd = v3_granule.data["agbd"]
+            v3_agbd_pi_upper = v3_granule.data["agbd_pi_upper"]
+            v3_agbd_pi_lower = v3_granule.data["agbd_pi_lower"]
+            self.assertTrue(
+                np.all(v3_agbd_pi_lower <= v3_agbd),
+                f"agbd_pi_lower is not less than agbd in V3 for {v3file.name}",
+            )
+            self.assertTrue(
+                np.all(v3_agbd <= v3_agbd_pi_upper),
+                f"agbd is not less than agbd_pi_upper in V3 for {v3file.name}",
+            )
+
+    def test_agbd_quality_flags(self):
+        print("\nWARNING: Currently omitting predict_stratum = None")
+        for _, file in self.file_pairs:
+            qflag = (
+                "l4_quality_flag"
+                if "V002" in file.name
+                else "l4a_quality_flag_rel3"
+            )
+            columns = [
+                "shot_number",
+                "agbd",
+                "predict_stratum",
+                qflag,
+            ]
+            granule = Granule(file, columns=columns)
+
+            # Check if agbd is not -9999 where quality flags are 1
+            valid_mask = granule.data[qflag] == 1
+            if "V003" in file.name:
+                valid_mask = np.logical_and(
+                    valid_mask, granule.data["predict_stratum"] != b"None"
+                )
+            self.assertTrue(
+                np.all(granule.data["agbd"][valid_mask] != -9999),
+                f"AGBD is -9999 where quality flag is 1 in V3 for {file.name}",
+            )
+
+    def test_agbd_mean_diff(self):
+        # I don't think this test is very meaningful.
+        big_diffs = []
+        for v2file, v3file in self.file_pairs:
+            columns = ["shot_number", "agbd"]
+            v2_granule = Granule(
+                v2file, columns=columns.append("l4_quality_flag")
+            )
+            v3_granule = Granule(
+                v3file, columns=columns.append("l4a_quality_flag_rel3")
+            )
+
+            # print mean difference in AGBD between v2 and v3
+            v2_agbd = v2_granule.data["agbd"]
+            v3_agbd = v3_granule.data["agbd"]
+            valid = np.logical_and(v3_agbd != -9999, v2_agbd != -9999)
+            mean_diff = np.mean(v3_agbd[valid] - v2_agbd[valid])
+
+            if np.abs(mean_diff) > 5:
+                print(
+                    f"Mean(V3_agbd - V2_agbd) for {v3file.name} and {v2file.name} is {mean_diff:.2f}"
+                )
+                big_diffs.append((v2file.name, v3file.name, mean_diff))
+
+        df = pd.DataFrame(
+            big_diffs,
+            columns=["v2file", "v3file", "mean_diff"],
+        )
+        # sort by mean diff
+        df = df.sort_values(by="mean_diff", ascending=False).to_csv(
+            "big_diffs.csv", index=False
+        )
+
+        big_diffs = sorted(big_diffs, key=lambda x: np.abs(x[2]), reverse=True)
+        for diff in big_diffs[:5]:
+            print(diff)
+
+    def test_same_predict_strata(self):
+        omit = ["O19336_04", "O19337_04"]
+        print(f"\nWARNING: Currently omitting orbits {omit}")
+        max_n = 0
+        max_fpair = None
+        for v2file, v3file in self.file_pairs:
+            if any([s in v2file.name for s in omit]):
+                continue
+            columns = ["shot_number", "predict_stratum"]
+            v2g = Granule(v2file, columns)
+            v3g = Granule(v3file, columns)
+            invalid = np.logical_and(
+                v2g.data["predict_stratum"] == b"",
+                v3g.data["predict_stratum"] == b"None",
+            )
+            # print differences in predict_stratum
+            v2ps = v2g.data["predict_stratum"][~invalid]
+            v3ps = v3g.data["predict_stratum"][~invalid]
+            if not np.array_equal(v2ps, v3ps):
+                diffs = v2ps != v3ps
+                v2p = v2ps[diffs]
+                v3p = v3ps[diffs]
+                n_diff = len(v2p)
+                if n_diff > max_n:
+                    max_n = n_diff
+                    max_fpair = (v2file.name, v3file.name)
+                n_tot = len(v2ps)
+                if self.verbosity >= 2:
+                    print(f"Differences: {n_diff/n_tot}")
+                self.assertLess(n_diff / n_tot, 0.004)
+
+                if self.verbosity >= 2:
+                    pairs = set(zip(v2p, v3p))
+                    print(pairs)
+        if self.verbosity >= 2:
+            print(("Max differences in predict_stratum:"
+                   f"{max_n} between {max_fpair[0]} and {max_fpair[1]}"
+            ))
+
+    def test_quality_flag_logic(self):
+        pass
+
+
+
+    def test_all_metadata_fields(self):
+        md = DATA_DICTIONARY_DIR / "product_dictionary_metadata.txt"
+        columns = _get_columns(md)
+        for _, file in self.file_pairs:
+            f = h5py.File(file, "r")
+            for col in columns:
+                if col not in f.keys():
+                    self.fail(f"Column {col} not found in {file.name}")
+    
+    def test_all_ancillary_fields(self):
+        ancillary = DATA_DICTIONARY_DIR / "product_dictionary_ancillary.txt"
+        columns = _get_columns(ancillary)
+        grps = set([col.split("/")[0] for col in columns])
+        grp_columns_dict = {
+            grp: [col.split("/")[1] for col in columns if col.startswith(grp + "/")]
+            for grp in grps
+        }
+        columns = [col.split("/") for col in columns]
+        for _, file in self.file_pairs:
+            g = Granule(file, columns=["agbd"])
+            for grp, name in columns:
+                if grp not in g.ancillary.keys():
+                    self.fail(f"Group {grp} not found in {file.name}")
+                if name not in g.ancillary[grp].columns:
+                    self.fail(f"Column {name} not found in group {grp} of {file.name}")
+            self.assertEqual(len(set(g.ancillary["model_data"]["predict_stratum"])), 35)
+            self.assertSetEqual(grps, set(g.ancillary.keys()),
+                              f"Ancillary groups in {file.name} do not match expected groups")
+        
+            for grp in grps:
+                self.assertSetEqual(
+                    set(g.ancillary[grp].columns), 
+                    set(grp_columns_dict[grp]),
+                    f"Columns in group {grp} of {file.name} do not match expected columns",
+                )
+                for col in g.ancillary[grp].columns:
+                    self.assertFalse(
+                        g.ancillary[grp][col].isnull().any(),
+                        f"Column {col} in group {grp} of {file.name} contains null values",
+                    )
+    
+    def test_all_beam_attributes(self):
+        beam_attrs = DATA_DICTIONARY_DIR / "product_dictionary_beam_attrs.txt"
+        columns = _get_columns(beam_attrs)
+        add = [
+            "elev_outlier_zscore_min",
+            "elev_outlier_tile_buffer",
+            "elev_outlier_zq",
+            "elev_outlier_zscore_t",
+            "elev_outlier_ncells",
+            "elev_outlier_tile_res",
+            "elev_outlier_zdiff",
+            "elev_outlier_tile_size",
+        ]
+        print("\nWARNING: Currently adding the following columns:")
+        print(add)
+        columns.extend(add)
+        for _, file in self.file_pairs:
+            f = h5py.File(file, "r")
+            for k in f.keys():
+                if k.startswith("BEAM"):
+                    attrs = f[k]["agbd_prediction"].attrs.keys()
+                    self.assertSetEqual(
+                        set(attrs),
+                        set(columns),
+                        f"Attributes for {k} in {file.name} do not match expected columns",
+                    )
+                
+    
+    def test_all_beam_fields(self):
+        omit = ["geolocation/degrade_flag", "land_cover_data/pft_class"]
+        print("\nWARNING: Currently omitting the following fields:")
+        print(omit)
+        beam_data = DATA_DICTIONARY_DIR / "product_dictionary_beam_data.txt"
+        columns = _get_columns(beam_data)
+        columns = [c for c in columns if c not in omit]
+        all_granules_all_zero_cols = { c: 0 for c in columns }
+        for _, file in self.file_pairs:
+            if self.verbosity >= 2:
+                print(f"Testing beam fields for {file.name}")
+            g = Granule(file, columns=columns)
+            dat = g.data
+            for col in dat.columns:
+                self.assertFalse(
+                    dat[col].isnull().all(),
+                    f"Column {col} of {file.name} is all null."
+                )
+                if is_numeric_dtype(dat[col]):
+                    if (dat[col] == 0).all():
+                        all_granules_all_zero_cols[col] += 1
+        for col, count in all_granules_all_zero_cols.items():
+            if count == len(self.file_pairs):
+                print(f"\nWARNING: column {col} is all zero in all granules.")
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tools/granule.py
@@ -0,0 +1,89 @@
+import pathlib
+from typing import List
+import h5py
+import numpy as np
+import pandas as pd
+
+
+class Granule(h5py.File):
+    """
+    A class to represent a GEDI granule.
+    """
+
+    def __init__(
+        self,
+        file_path: pathlib.Path,
+        columns: List[str],
+        quality_filter: bool = False,
+    ):
+        super().__init__(file_path, "r")
+        self.beams = list([k for k in self.keys() if k.startswith("BEAM")])
+        if not columns:
+            # TODO: Ideally, get all columns if none are specified
+            raise NotImplementedError
+        if "shot_number" not in columns:
+            columns.append("shot_number")
+        if quality_filter:
+            if file_path.name.endswith("V002.h5"):
+                if "l4_quality_flag" not in columns:
+                    columns.append("l4_quality_flag")
+            elif file_path.name.endswith("V003.h5"):
+                if "l4a_quality_flag_rel3" not in columns:
+                    columns.append("l4a_quality_flag_rel3")
+                # Currently working around a bug in the V003 files where
+                # the predict_stratum is sometimes not present but quality flag
+                # is True.
+                if "predict_stratum" not in columns:
+                    columns.append("predict_stratum")
+
+        self.columns = columns
+        self.data = self.iter_beams()
+        self.ancillary = self.iter_ancillary()
+        if quality_filter:
+            if "l4_quality_flag" in self.data.columns:
+                self.data = self.data[self.data["l4_quality_flag"] == 1]
+            elif "l4a_quality_flag_rel3" in self.data.columns:
+                # Currently working around a bug in the V003 files where
+                # the predict_stratum is sometimes not present but quality flag
+                # is True.
+                self.data = self.data[
+                    (self.data["l4a_quality_flag_rel3"] == 1)
+                    & (self.data["predict_stratum"] != b'None')
+                ]
+
+    def iter_ancillary(self):
+        """
+        Iterate over the ancillary data in the granule and extract data.
+        """
+        ancillary_data = {}
+        for key in self["ANCILLARY"]:
+            d = self["ANCILLARY"][key]
+            if isinstance(d, h5py.Dataset):
+                columns = self["ANCILLARY"][key].dtype.fields.keys()
+                dic = {col: list(d[col][:]) for col in columns}
+                df = pd.DataFrame(dic)
+            ancillary_data[key] = df
+        return ancillary_data
+
+    def iter_beams(self):
+        """
+        Iterate over the beams in the granule and extract data.
+        """
+        data = []
+        for beam in self.beams:
+            beam_data = {}
+            for col in self.columns:
+                if col in self[beam]:
+                    d = self[beam][col][:]
+                    if len(d.shape) == 1:
+                        beam_data[col] = d
+                    else:
+                        beam_data[col] = list(d)
+                else:
+                    raise ValueError(f"Requested field {col} not found.")
+            beam_data["beam"] = beam
+            data.append(beam_data)
+        data = pd.concat([pd.DataFrame(d) for d in data], ignore_index=True)
+        data.set_index("shot_number", inplace=True, drop=False)
+
+        return data
